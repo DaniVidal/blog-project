7211a7d7c527e019b44e614de5f71ac6c1dd0aa1
Daniele Vidal <danielevidal@MacBook-Pro-de-Daniele.local>
2024-02-08 20:26:39 -0300

    Adding react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Home } from './pages/Home';
@@ -8,6 +14,18 @@ import { Post } from './pages/Post';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/post/:slug">
+          <Post />
+        </Route>
+        <Route path="*">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
 )
